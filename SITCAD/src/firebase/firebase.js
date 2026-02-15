6e499be6ae9005eba58b1dd0e91e5d712c3d5e71
Anxiietyx <ivanloong05@gmail.com>
2026-02-15 11:47:03 +0800

    Installation of firebase

---
+++ SITCAD/src/firebase/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBiGnGeMcAegfwVVgaBLJq8LHOzUbJwqEI",
+  authDomain: "sitcad-capstone.firebaseapp.com",
+  projectId: "sitcad-capstone",
+  storageBucket: "sitcad-capstone.firebasestorage.app",
+  messagingSenderId: "777589780638",
+  appId: "1:777589780638:web:5da973753a34229b604b8f",
+  measurementId: "G-GXYG3C8G5J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export default {auth, app};
